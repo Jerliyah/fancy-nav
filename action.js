63c341c34c6d63660a640bdd7fbda933f5a6d0cb
Jerliyah <jerliyahcraig@gmail.com>
2018-02-05 20:06:40 -0600

    success! but there may be browser issues...

--- action.js
+++ action.js
@@ -1,13 +1,29 @@
 /* ====== DOM Grab ====== */
-
+nav = document.querySelector('nav');
+logo = document.querySelector('#logo');
 
 
 /* ====== Variables ====== */
+var initial_nav_top = nav.getBoundingClientRect().top
 
 
 
 /* ====== Functions ====== */
+function sticky_nav() {
+    // console.log(window.scrollY)
+    // console.log( initial_nav_top)
 
+    if( window.scrollY >= initial_nav_top ) {
+        nav.style.position = "fixed";
+        logo.classList.remove('hidden')
+    }
+    else {
+        nav.style.position = "relative";
+        logo.classList.add('hidden')
+    }
+    
+}
 
 
 /* ====== Events ====== */
+window.addEventListener('scroll', sticky_nav)
